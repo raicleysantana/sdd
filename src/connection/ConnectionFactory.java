70cfc01a23fa2610ae5a23ebf733db42bbff9e57
Raicley Santana <raicleysantana1@gmail.com>
2020-08-31 23:13:31 -0400

    initial commit

---
+++ src/connection/ConnectionFactory.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package connection;
+
+/**
+ *
+ * @author Raicley Santana
+ */
+public class ConnectionFactory {
+    private static final String DRIVER = "com.mysql.jdbc.Driver";
+    private static final String URL = "jdbc:postgresql://localhost/sdd";
+    private static final String USER = "postgre";
+    private static final String PASS = "123";
+}
